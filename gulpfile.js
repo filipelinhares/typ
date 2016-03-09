9e60082f41ffb3da2c093bfe8f35c609c57caaf6
Filipe Linhares <lip.linhares@gmail.com>
2016-03-09 00:30:16 -0300

    Uses cssnext instead of SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
-var gulp         = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var csso         = require('gulp-csso');
-var sass         = require('gulp-sass');
-var rename       = require('gulp-rename');
-var clean        = require('gulp-clean');
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var shrink = require('gulp-cssshrink');
+var del = require('del');
 
-gulp.task('compile', function () {
-  return gulp.src('src/*.scss')
-         .pipe(sass())
-         .pipe(autoprefixer())
-         .pipe(gulp.dest('dist/'));
+var postCSSArray = [
+  require('autoprefixer')(),
+  require('cssnext')(),
+  require('postcss-reporter')(),
+];
+
+gulp.task('clean', function() {
+  return del(['dist']);
 });
 
-gulp.task('clean', function () {
-  return gulp.src('dist/')
-         .pipe(clean({force: true}))
+gulp.task('postcss', ['clean'], function () {
+  return gulp.src(['lib/typ.css', 'lib/typ-rhythm.css'])
+    .pipe(postcss(postCSSArray))
+    .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('min', ['compile', 'clean'], function () {
-  return gulp.src('dist/*.css')
-         .pipe(csso())
-         .pipe(rename({suffix: '.min'}))
-         .pipe(gulp.dest('dist/'));
+gulp.task('min', ['postcss'], function () {
+    return gulp.src(['dist/typ.css', 'dist/typ-rhythm.css'])
+    .pipe(shrink())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('default', ['compile', 'min']);
+gulp.task('watch', function() {
+  gulp.watch('lib/*.css', ['postcss']);
+});
+
+gulp.task('default', ['postcss', 'min'] )
